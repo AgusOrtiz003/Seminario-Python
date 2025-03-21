2abd929e61bf70d54ce8ba143a7a89131f75f1b1
Agustin Ortiz <agusortiz003@gmail.com>
2025-03-21 14:16:00 -0300

    Ejercicio extra de la práctica 1

---
+++ p1pextra.py
@@ -0,0 +1,24 @@
+
+productos={}
+opcion=-1
+while(opcion!=0):
+    print("Bienvenido a la base de datos del kiosco, seleccione una opción:")
+    print("1 Agregar un producto"+"\n2 Eliminar un producto"+ "\n3 Mostrar inventario"+"\n0 Salir del programa")
+    opcion=(int(input()))
+    match opcion:
+        case 1:
+                nom=str(input("Ingrese el nombre del producto: "))
+                if (nom not in productos):
+                     productos[nom]=int(input("Ingrese la cantidad inicial del producto: "))
+                else:
+                     println("Error: El producto ya existe")
+        case 2:
+                nom=input("Ingrese el nombre del producto a eliminar: ")
+                if (nom in productos):
+                      del productos[nom]
+                else: println("Error: El producto no existe")
+        case 3:
+                for key in productos:
+                      print(f"|Producto: {key} | Cantidad: {productos[key]}|")
+        case 0:
+                print("Hasta luego")
